49fb127a5b57216b25cb64d243973cebc69bd2bf
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-07-15 15:48:04 +0900

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_065644) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.integer "photos"
     t.float "rental_price_day"
     t.integer "category"
     t.datetime "created_at", null: false
