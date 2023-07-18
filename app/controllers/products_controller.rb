1d447e9c4f4a682ba973dfb374e7a24047a9d341
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-07-18 21:50:01 +0900

    new product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.user = current_user
     if @product.save
       redirect_to @product, notice: "Product was successfully added!"
     else
@@ -23,7 +24,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :category, photos: [])
+    params.require(:product).permit(:name, :description, :rental_price_day, :category, photos: [])
   end
 
   def set_product
