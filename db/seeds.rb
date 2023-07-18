1b2b9b84858adaa48737d48e0bd9a9ef7c0b9f82
光川聖恵 <kiyo0708mk@gmail.com>
2023-07-18 20:04:11 +0900

    created user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+User.destroy_all
+
+puts "Creating Users..."
+
+10.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: "0123456"
+  )
+  puts "User with id :#{user.id} has been created"
+end
+puts "Finished!"
+
+# puts "Cleaning database..."
+# Product.destroy_all
+
+# puts "Creating Products..."
+
+# 20.times do
+#   product = Product.create!(
+#     name: Faker::Internet.email,
+#     description: Faker::Internet.password
+#     rental_price_day:
+#     category:
+#   )
+#   puts "Product with id :#{product.id} has been created"
+
+# end
+# puts "Finished!"
