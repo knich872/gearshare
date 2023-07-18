11ff656f6ba344e2ccf3211019f95942a502bfe2
Benjamin How <corinthian8@gmail.com>
2023-07-18 18:56:28 +0900

    validation added for products and bookings, user  hasn't been done because I assume this is done by devise....also, user doesn't have a name field

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,12 @@ class Booking < ApplicationRecord
   belongs_to :product
   belongs_to :user
   enum staus: [ :pending, :accepted, :rejected, :canceled, :complete ]
+  validates :start_date, :end_date, :user, :product, presence: true
+  validate :valid_dates
+
+  private
+
+  def valid_dates
+    errors.add(:start_date, 'must be before end date') if start_date >= end_date
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   has_many :bookings
   has_one :user
   enum category: [ :cameras, :clothes, :appliances, :jewelry, :sports, :outdoors, :events, :transportation, :tools, :baby ]
+  validates :name, :description, :rental_price_day, :category, presence: true
+  validates :rental_price_day, numericality: { greater_than: 0 }
 end
