8dadbd88f708031d0b4bee6ff2660e04103235d7
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-07-18 20:39:45 +0900

    adds photos as attachments to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :category)
+    params.require(:product).permit(:name, :description, :price, :category, photos: [])
   end
 
   def set_product
