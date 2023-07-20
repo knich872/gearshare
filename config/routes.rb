7fe60268f1416a7d5528d37fa3e229ae4c843e16
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-07-20 19:49:49 +0900

    added category routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,25 @@ Rails.application.routes.draw do
   get 'poducts/index'
   get 'poducts/show'
   devise_for :users
-  root to: "pages#home"
+  root to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :products, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:new, :create]
+    collection do
+      get :cameras
+      get :clothes
+      get :appliances
+      get :jewelry
+      get :sports
+      get :outdoors
+      get :events
+      get :transportation
+      get :tools
+      get :baby
+    end
   end
 
   # a regular user can see the bookings they've made
