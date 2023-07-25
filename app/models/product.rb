be5d2f3e5bba8a95313868cd35e3cdf840c0700a
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-07-25 18:17:26 +0900

    search bar added

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,12 @@ class Product < ApplicationRecord
   enum category: [:cameras, :clothes, :appliances, :jewelry, :sports, :outdoors, :events, :transportation, :tools, :baby]
   validates :name, :description, :rental_price_day, :category, presence: true
   validates :rental_price_day, numericality: { greater_than: 0 }
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_keywords,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
