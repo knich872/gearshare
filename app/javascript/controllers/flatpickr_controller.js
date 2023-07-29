5c31e2e6213bf1146b3a42aab59a426ce968f3e0
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-07-29 12:01:22 +0900

    flatpickr controller updates total amount

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,17 +1,16 @@
+// app/javascript/controllers/flatpickr_controller.js
+
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
-// Import the rangePlugin from the flatpickr library
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-// Connects to data-controller="flatpickr"
 export default class extends Controller {
-  // Inform the controller that it has two targets in the form, which are our inputs
-  static targets = [ "startTime", "endTime" ]
+  static targets = ["startTime", "endTime", "rentalPriceValue", "totalAmount"]
 
   connect() {
+    console.log("Hello from flatpickr controller");
     flatpickr(this.startTimeTarget, {
-      enableTime: false,
-      plugins: [new rangePlugin({ input: "#end_time"})],
+      plugins: [new rangePlugin({ input: "#end_time" })],
       defaultDate: "today",
       minDate: "today",
       altInput: true,
@@ -21,5 +20,23 @@ export default class extends Controller {
       minDate: "today",
       altInput: true,
     })
+    this.calculateAmount();
+  }
+
+  calculateAmount() {
+    const startDate = this.startTimeTarget.value;
+    console.log(new Date(startDate));
+    const endDate = this.endTimeTarget.value;
+    console.log(endDate);
+    const rentalPrice = this.element.querySelector("[data-rental-price-value]").dataset.rentalPriceValue;
+    console.log(rentalPrice);
+    if (startDate && endDate) {
+      const daysDiff = Math.ceil((new Date(endDate) - new Date(startDate)) / (1000 * 60 * 60 * 24)) + 1;
+      const totalAmount = rentalPrice * daysDiff;
+
+      this.totalAmountTarget.textContent = `¥${totalAmount}`;
+    } else {
+      this.totalAmountTarget.textContent = `¥${rentalPrice}`;
+    }
   }
 }
